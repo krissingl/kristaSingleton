4f55046f2e3be89392e8627ed5a6fedf0ad9f769
Krista Singleton <kris.singlet231@gmail.com>
2020-11-12 22:22:21 -0700

    added components folder and contact component

--- website/src/App.js
+++ website/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Contact from './components/contact.js'
 
 function App() {
   return (
@@ -6,6 +7,7 @@ function App() {
       <h3>
         Thanks for Visiting
       </h3>
+      <Contact />
     </div>
   );
 }
---
+++ website/src/components/contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contact = () => (
+  <div>
+    <a href="https://www.linkedin.com/in/krista-singleton/" className="contact">LinkedIn</a>
+    <br></br>
+    <a href="https://github.com/krissingl" className="contact">Github</a>
+  </div>
+);
+
+export default Contact;
