ef520295ebc217f018571cf49e35f8d9121106f5
Krista Singleton <kris.singlet231@gmail.com>
2020-11-15 22:29:06 -0700

    Changed overall styling a bit and added styletoggle buttons for style change feature

--- website/src/App.js
+++ website/src/App.js
@@ -4,10 +4,12 @@ import Contact from './components/contact.js'
 import AboutMe from './components/aboutMe.js'
 import Portfolio from './components/portfolio.js'
 import Resume from './components/resume.js'
+import StyleToggle from './components/styleToggle.js'
 
 function App() {
   return (
     <div className="App">
+      <StyleToggle />
       <Title />
       <Contact />
       <AboutMe />
---
+++ website/src/components/styleToggle.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const StyleToggle = () => (
+  <div className="styleToggle">
+    <button>Business</button>
+    <button>Fun</button>
+  </div>
+);
+
+export default StyleToggle;
