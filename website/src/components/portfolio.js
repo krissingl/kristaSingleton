98234ae73b34eaf0af89c05070f8b15ff479d4e1
Krista Singleton <kris.singlet231@gmail.com>
2020-11-15 22:23:16 -0700

    Added project link for LetsTalk and styling

--- website/src/components/portfolio.js
+++ website/src/components/portfolio.js
@@ -3,7 +3,29 @@ import React from 'react';
 const Portfolio = () => (
   <div className="component">
     <h5>Projects</h5>
-    <p className="portfolio">Porfolio stuff goes here</p>
+    <div className="portfolioList">
+      <a href="https://lets-talk231.herokuapp.com/" className="portfolioProjectLink">
+          <img className="portfolioProjectImg" src="extras/LetsTalkLogo.png" alt="LetsTalk"></img>
+          <div className="overlay">
+            <div className="overlayMsg">Visit Project Website</div>
+          </div>
+      </a>
+      <br></br>
+      <a href="https://lets-talk231.herokuapp.com/" className="portfolioProjectLink">
+          <img className="portfolioProjectImg" src="extras/LetsTalkLogo.png" alt="LetsTalk"></img>
+          <div className="overlay">
+            <div className="overlayMsg">Visit Project Website</div>
+          </div>
+      </a>
+      <br></br>
+      <a href="https://lets-talk231.herokuapp.com/" className="portfolioProjectLink">
+          <img className="portfolioProjectImg" src="extras/LetsTalkLogo.png" alt="LetsTalk"></img>
+          <div className="overlay">
+            <div className="overlayMsg">Visit Project Website</div>
+          </div>
+      </a>
+      <br></br>
+    </div>
   </div>
 );
 
