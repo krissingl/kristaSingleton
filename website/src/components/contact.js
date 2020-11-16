7e19ee6e3a586dd2418e7846edf800c6c0775373
Krista Singleton <kris.singlet231@gmail.com>
2020-11-15 21:49:28 -0700

    Fixed contact links and added clickable icons. Added email hyperlink

--- website/src/components/contact.js
+++ website/src/components/contact.js
@@ -3,12 +3,16 @@ import React from 'react';
 const Contact = () => (
   <div>
     <div className="contactLinks">
-      <a href="https://www.linkedin.com/in/krista-singleton/" className="contact">
-        <img src="./extras/linkedin.png" alt="Linkedin"></img>
+      <a href="https://www.linkedin.com/in/krista-singleton/">
+        <img className="contactImg" src="extras/linkedin.png" alt="Linkedin"></img>
       </a>
       <br></br>
-      <a href="https://github.com/krissingl" className="contact">
-        <img src="./../../linkedin.png" alt="Github"></img>
+      <a href="https://github.com/krissingl">
+        <img className="contactImg" src="extras/github.png" alt="Github"></img>
+      </a>
+      <br></br>
+      <a href="mailto:kris.singlet231@gmail.com">
+        <img className="contactImg" src="extras/envelope.png" alt="Linkedin"></img>
       </a>
     </div>
   </div>
